d1264af356615df5c589627f74e296ac35466b6d
Pouria <pouria.msd@gmail.com>
2023-04-20 14:05:34 +0200

    add functionality to the keyboard

--- src/components/Key.jsx
+++ src/components/Key.jsx
@@ -1,6 +1,19 @@
+import { useContext } from "react";
+import { AppContext } from "../App";
+
 const Key = ({ keyVal, bigKey }) => {
+  const { onDelete, onEnter, onSelectLetter } = useContext(AppContext);
+  const selectLetter = () => {
+    if (keyVal === "ENTER") {
+      onEnter();
+    } else if (keyVal === "DELETE") {
+      onDelete();
+    } else {
+      onSelectLetter(keyVal);
+    }
+  };
   return (
-    <div className="key" id={bigKey && "big"}>
+    <div className="key" id={bigKey && "big"} onClick={selectLetter}>
       {keyVal}
     </div>
   );
