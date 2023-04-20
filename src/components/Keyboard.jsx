76df13b72218b27c7dd5cc588687cc2b113d3057
Pouria <pouria.msd@gmail.com>
2023-04-20 14:44:42 +0200

    writing functionality for detecting the actual keyboard

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -1,3 +1,5 @@
+import { useCallback, useEffect, useContext } from "react";
+import { AppContext } from "../App";
 import Key from "./Key";
 
 const Keyboard = () => {
@@ -5,19 +7,53 @@ const Keyboard = () => {
   const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
   const keys3 = ["Z", "X", "C", "V", "B", "N", "M"];
 
+  const { onDelete, onEnter, onSelectLetter } = useContext(AppContext);
+
+  const handleKeyboard = useCallback((event) => {
+    if (event.key === "Enter") {
+      onEnter();
+    } else if (event.key === "Backspace") {
+      onDelete();
+    } else {
+      keys1.forEach((key) => {
+        if (event.key.toLowerCase() === key.toLowerCase()) {
+          onSelectLetter(key);
+        }
+      });
+      keys2.forEach((key) => {
+        if (event.key.toLowerCase() === key.toLowerCase()) {
+          onSelectLetter(key);
+        }
+      });
+      keys3.forEach((key) => {
+        if (event.key.toLowerCase() === key.toLowerCase()) {
+          onSelectLetter(key);
+        }
+      });
+    }
+  });
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyboard);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyboard);
+    };
+  }, [handleKeyboard]);
+
   return (
-    <div className="keyboard">
-      <div className="line1" >
+    <div className="keyboard" onKeyDown={handleKeyboard}>
+      <div className="line1">
         {keys1.map((key) => {
           return <Key keyVal={key} />;
         })}
       </div>
-      <div className="line2" >
+      <div className="line2">
         {keys2.map((key) => {
           return <Key keyVal={key} />;
         })}
       </div>
-      <div className="line3" >
+      <div className="line3">
         <Key keyVal={"ENTER"} bigKey />
         {keys3.map((key) => {
           return <Key keyVal={key} />;
