7e7af11687f965594ccd8c7a174ae04871646700
Pouria <pouria.msd@gmail.com>
2023-04-20 13:12:22 +0200

    Creating the keyboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
 import { createContext, useState } from "react";
-import "./App.css"
+import "./App.css";
 import Board from "./components/Board";
 import Keyboard from "./components/Keyboard";
 import { boardDefault } from "./Words";
 
-export const AppContext = createContext()
+export const AppContext = createContext();
 
 function App() {
-  const [board, setBoard]= useState(boardDefault)
-  return <div className="App">
-    <nav>
-      <h1>Wordle</h1>
-    </nav>
-    <AppContext.Provider value={{board, setBoard}}>
-      <Board/>
-      <Keyboard/>
-    </AppContext.Provider>
-  </div>;
+  const [board, setBoard] = useState(boardDefault);
+  return (
+    <div className="App">
+      <nav>
+        <h1>Wordle</h1>
+      </nav>
+      <AppContext.Provider value={{ board, setBoard }}>
+        <div className="game">
+          <Board />
+          <Keyboard />
+        </div>
+      </AppContext.Provider>
+    </div>
+  );
 }
 
 export default App;
