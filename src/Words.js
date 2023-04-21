384d380f52bee0305bc8533a2ae8b046124fab5e
Pouria <pouria.msd@gmail.com>
2023-04-21 12:27:03 +0200

    creating random word for each game

--- src/Words.js
+++ src/Words.js
@@ -11,11 +11,13 @@ export const boardDefault = [
 
 export const generateWordSet = async () => {
   let wordSet;
+  let todaysWork;
   await fetch(wordBank)
     .then((response) => response.text())
     .then((result) => {
       const wordArr = result.split("\n");
+      todaysWork = wordArr[Math.floor(Math.random() * wordArr.length)];
       wordSet = new Set(wordArr);
     });
-  return { wordSet };
+  return { wordSet, todaysWork };
 };
